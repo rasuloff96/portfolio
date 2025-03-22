f7fa99a4c42bac95f11dd13ebc71436395f27edf
Asilbeck <asildev96@gmail.com>
2025-03-22 23:58:34 +0300

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-full bg-red-800 rounded'>
-      a
+    <div className='w-full flex gap-2'>
+      <div className='bg-slate-800 text-white w-1/2 h-auto rounded p-3'>
+        <h2>Asilbek Rasulov</h2>
+        <p>Assalamu Alaykum. Shaxsiy Portfolioga xush kelibsiz. Bu yerda siz men haqimda malumot olishingiz mumkun va bundan tashqari men bilan bog'lanishingiz va mening ishlarimni korib chiqishingiz mumkun!!!</p>
+        
+      </div>
+
+      <div className='w-1/2 text-white flex flex-col gap-2 h-auto'>
+
+        <div className='p-3 bg-slate-800  rounded'> <p>My skills</p> </div>
+        <div className='p-3 bg-slate-800  rounded'> <p>contact with me</p> </div>
+
+      </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/Logo.webp"
 
 const Navbar = () => {
     return (
-        <nav className="fixed top-0 left-0 w-full p-4 h-18 bg-neutral-950 shadow-md flex items-center">
+        <nav className="fixed top-0 left-0 w-full p-4 h-18 bg-slate-800 shadow-md flex items-center">
             <div className="flex w-full items-center space-x-4 justify-between">
                 <img src={logo} alt="logo in here" className="h-10 rounded" />
                 <ul className="flex justify-between text-white gap-3">

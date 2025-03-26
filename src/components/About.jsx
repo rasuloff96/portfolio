b196896c2fe1e9ff2ee4411f7071facc3d30412b
Asilbeck <asildev96@gmail.com>
2025-03-26 23:32:14 +0300

    ...

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='bg-slate-800 text-white w-full h-auto rounded p-3'>About</div>
+    <div className='bg-slate-800 text-white w-full h-auto rounded p-3'>Bu yerda men haqimda barcha malumotlar keltiriladi</div>
   )
 }
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='bg-slate-800 text-white w-full h-auto rounded p-3'>Contact</div>
+    <div className='bg-slate-800 text-white w-full h-auto rounded p-3'>Bu yerda men bilan boglanish uchun bir nechta havolalar bolishi kerag</div>
   )
 }
 
